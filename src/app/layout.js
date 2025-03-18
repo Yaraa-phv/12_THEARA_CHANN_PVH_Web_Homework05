81206db7ec940f6bf2db7fd20babf677a4a8a9ba
Theara <thearachann134@gmail.com>
2025-03-18 14:50:22 +0700

    config tailwind and heroui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
+import App from "@/components/App";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <App/>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <>
-    
+    <div className="text-3xl font-bold bg-red-900">
+      Hello world!
+    </div>
     </>
-  );
+  )
 }
